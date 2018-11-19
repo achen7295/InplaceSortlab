627dc5d00b35e86a6a1580c64787440e6698c6b6
BT_1N3_30 <achen7295@bths.edu>
2018-11-19 11:30:19 -0500

    Sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,23 +3,32 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
 
-    public static void insertionsort(int [] arr){
-        int j;
-        for(int i = 0; i < arr.length; i++){
-            j = i;
-            while(j > 0 && arr[j-1] > arr[j]){
-                swap(arr, j, j-1);
-                j = j - 1;
-            }
-        }
-    }
+        InPlaceSorts z = new InPlaceSorts();
 
-    public static void swap(int arr[],int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+
+        int[] intarr = z.randomIntArr(10000);
+        double[] Doublearr = z.IntToDouble(intarr);
+        String[]  Stringarr = z.IntToString(intarr);
+
+
+        long time = System.nanoTime();
+        z.insertionSort(intarr);
+        time = System.nanoTime() - time;
+        System.out.println("Time Taken - Insertion Sort: "+ time + " nano seconds");
+
+        long time2 = System.nanoTime();
+        z.selectionsort(Doublearr);
+        time2 = System.nanoTime() - time2;
+        System.out.println("Time Taken - Selection Sort: "+ time2 + " nano seconds");
+
+
+        long time3 = System.nanoTime();
+        z.bubbleSort(Stringarr);
+        time3 = System.nanoTime() - time3;
+        System.out.println("Time Taken - Bubble Sort: "+ time3 + " nano seconds");
+
+
+
     }
 }
